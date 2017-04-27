49032874cf7d319d73f18802b903d3ea5aabcb02
Leonardo Quatrin Campagnolo <campagnolo.lq@gmail.com>
2017-04-27 19:10:43 -0300

    svd test

--- svddocsprojection.py
+++ svddocsprojection.py
@@ -17,18 +17,23 @@ import numpy as np
 import os
 import gc
 
-def SVDDocsProjection (data_doc, mtx_original_distance, N, d, number_of_documents, path):
+def SVDDocsProjection (documents, mtx_original_distance, N, d, number_of_documents, path):
+  # Create DataDocs
+  data_doc = np.zeros(shape = (number_of_documents, d))
+  for doc_id in range(number_of_documents):
+    for word_id, count_w in documents[doc_id].items():
+      data_doc[doc_id, word_id] = float(count_w)
+    
   print('SVD Case', N)
          
-  text_file = open(path + str(N) + "_svd_docs.txt", "w")
-  text_file.write("%s\t%s\t%s\t%s\n" % ('svd_time', 'proj_time', 'dist_time', 'distortion'))
-
+  print("SVD")
   # Call SVD for sparse matrices
   s_clock = time.clock()
   U, S, Vt = linalg.svds(data_doc, k = N, which = 'LM')
   f_clock = time.clock()
   svd_time = (f_clock - s_clock)
 
+  print("Generate Original Matrix")
   s_clock = time.clock()
   # Correct diagonal
   # linalg.svds returns eigenvalues on Ascending Order    
@@ -39,20 +44,30 @@ def SVDDocsProjection (data_doc, mtx_original_distance, N, d, number_of_document
   #   space to your original d-dimensional space. If you don't need to map
   #   it back, just leave V out, and done you are.
   # We will not correct the colunm content, because the distance value will be the same.
-  projection = np.dot(U, S)
-  #projection = np.dot(projection, Vt)
+  data_doc = np.dot(U, np.dot(S, Vt))
+  #data_doc = np.dot(projection, Vt)
 
   f_clock = time.clock()
   proj_time = (f_clock - s_clock)
 
+  #http://scikit-bio.org/docs/0.2.0/generated/skbio.stats.distance.html
+  #http://stackoverflow.com/questions/1871536/euclidean-distance-between-points-in-two-different-numpy-arrays-not-within
+  #https://docs.scipy.org/doc/scipy-0.19.0/reference/spatial.distance.html
+  #http://stackoverflow.com/questions/22720864/efficiently-calculating-a-euclidean-distance-matrix-using-numpy
+  #https://docs.scipy.org/doc/scipy-0.19.0/reference/spatial.distance.html
+  print("Euclidean Distance")
   s_clock = time.clock()
-  #projected_distances = sp.distance.pdist(projection.T, metric='euclidean')
-  projected_distances, s_time = bwdistance.DoEuclidianDistanceProjDocs(projection.T)
+  #projected_distances = sp.distance.pdist(data_doc.T, metric='euclidean')
+  projected_distances, s_time = bwdistance.DoEuclidianDistanceProjDocs(data_doc.T)
   f_clock = time.clock()
   dist_time = (f_clock - s_clock)
-  
+
+  print("Distortion")
   max_distortion = bwmath.MaxDistortion(mtx_original_distance, projected_distances)
   print(max_distortion)
+
+  text_file = open(path + str(N) + "_svd_docs.txt", "w")
+  text_file.write("%s\t%s\t%s\t%s\n" % ('svd_time', 'proj_time', 'dist_time', 'distortion'))
 
   text_file.write("%f\t%f\t%f\t%f\n" % (svd_time, proj_time, dist_time, max_distortion))
 
