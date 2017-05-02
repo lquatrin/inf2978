ea11e8f47c14bfce2a4592f4a60b819b31ee039d
Leonardo Quatrin Campagnolo <campagnolo.lq@gmail.com>
2017-05-01 23:29:39 -0300

    svddoc

--- svddocsprojection.py
+++ svddocsprojection.py
@@ -38,64 +38,81 @@ def MaxDistortionSVD(original_distance, projected_distance):
     return max_distortion, max_strech
 
 def SVDDocsProjection (documents, mtx_original_distance, N, d, number_of_documents, path):
-  data_doc = None
+  A = None
   if isinstance(documents, dict):
     # Create DataDocs
-    data_doc = np.zeros(shape = (number_of_documents, d))
+    A = np.zeros(shape = (number_of_documents, d))
     for doc_id in range(number_of_documents):
       for word_id, count_w in documents[doc_id].items():
-        data_doc[doc_id, word_id] = float(count_w)
+        A[doc_id, word_id] = float(count_w)
   elif isinstance(documents, np.ndarray):
-    data_doc = documents.T
+    A = documents.T
         
-  print('SVD Case', N, data_doc.shape)
+  print('SVD Case', N, A.shape)
          
-  print("SVD")
   # Call SVD for sparse matrices
   s_clock = time.clock()
   #U, S, Vt = np.linalg.svd(data_doc, full_matrices = False)
-  U, S, Vt = linalg.svds(data_doc, k = N, which = 'LM')
+  U, vS, Vt = linalg.svds(A, k = N, which = 'LM')
   f_clock = time.clock()
-  svd_time = (f_clock - s_clock)
 
+  print('- SVD Time: ', (f_clock - s_clock))
+
+  #Generating A_k
   s_clock = time.clock()
-  S = np.diag(S)
-
-  # https://stats.stackexchange.com/questions/107533/how-to-use-svd-for-dimensionality-reduction-to-reduce-the-number-of-columns-fea
-  # Matrix V is used only to map the data from this reduced n-dimensional
-  #   space to your original d-dimensional space. If you don't need to map
-  #   it back, just leave V out, and done you are.
-  # We will not correct the colunm content, because the distance value will be the same.
-  #data_doc = np.dot(np.dot(U, S), Vt)
-  #data_doc = np.dot(projection, Vt)
-  #dunno
-  data_doc = data_doc - np.dot(np.dot(U, S), Vt)
-  
-  #print(max_distortion)
+  S = np.diag(vS)
+  A_k = np.dot(np.dot(U, S), Vt)
   f_clock = time.clock()
-  proj_time = (f_clock - s_clock)
+  print('- Ak  Time: ', (f_clock - s_clock))
+
+  #Quality Measure 1:
+  # 1 - (||A_k - A|| / ||A||)
+  s_clock = time.clock()
+
+  norma_frob = np.linalg.norm(A_k - A)
+  norma_frob_orig = np.linalg.norm(A)
+  quality_1 = 1.0 - (norma_frob / norma_frob_orig)
+  f_clock = time.clock()
+
+  print('- Quality 1: ', (f_clock - s_clock))
+  print('  ', quality_1 * 100.0)
+  
+  #Quality Measure 2
+  # 100 * ( (soma_sig)**2 / frob(A)**2)
+  s_clock = time.clock()
+  quality_2 = 100.0 * (np.sum(vS)**2 / (norma_frob_orig**2))
+  f_clock = time.clock()
+
+  print('- Quality 2: ', (f_clock - s_clock))
+  print('  ', quality_2)
+
+  #Error Measure  
+  s_clock = time.clock()
+
+  f_clock = time.clock()
+
 
   #http://scikit-bio.org/docs/0.2.0/generated/skbio.stats.distance.html
   #http://stackoverflow.com/questions/1871536/euclidean-distance-between-points-in-two-different-numpy-arrays-not-within
   #https://docs.scipy.org/doc/scipy-0.19.0/reference/spatial.distance.html
   #http://stackoverflow.com/questions/22720864/efficiently-calculating-a-euclidean-distance-matrix-using-numpy
   #https://docs.scipy.org/doc/scipy-0.19.0/reference/spatial.distance.html
-  print("Euclidean Distance")
-  s_clock = time.clock()
+  #print("Euclidean Distance")
+  #s_clock = time.clock()
   #projected_distances = sp.distance.pdist(data_doc.T, metric='euclidean')
   #projected_distances, s_time = bwdistance.DoEuclidianDistanceProjDocs(data_doc.T)
   #projected_distances = pdist(data_doc, 'sqeuclidean')
 
-  print("Distortion")
-  max_distortion = np.linalg.norm(data_doc)
+  #print("Distortion")
+  #max_distortion = np.linalg.norm(data_doc)
   #max_distortion, mst = MaxDistortionSVD(mtx_original_distance, projected_distances)
   #print(max_distortion, mst)
 
-  f_clock = time.clock()
-  dist_time = (f_clock - s_clock)
+  #f_clock = time.clock()
+  #dist_time = (f_clock - s_clock)
 
-  text_file = open(path + str(N) + "_svd_docs.txt", "w")
-  text_file.write("%s\t%s\t%s\t%s\n" % ('svd_time', 'proj_time', 'dist_time', 'frobenius'))
-  text_file.write("%f\t%f\t%f\t%f\n" % (svd_time, proj_time, dist_time, max_distortion))
+  #text_file = open(path + str(N) + "_svd_docs.txt", "w")
+  #text_file.write("%s\t%s\t%s\t%s\n" % ('svd_time', 'proj_time', 'dist_time', 'frobenius'))
+  #text_file.write("%f\t%f\t%f\t%f\n" % (svd_time, proj_time, dist_time, max_distortion))
   
-  text_file.close()
+  #text_file.close()
