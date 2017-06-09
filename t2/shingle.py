967dc6f47bfcba8613ac964aa1d4e58e2755a046
Leonardo Quatrin Campagnolo <campagnolo.lq@gmail.com>
2017-06-09 19:18:25 -0300

    ajustando o arquivo do shingle para testes

--- t2/shingle.py
+++ t2/shingle.py
@@ -29,10 +29,10 @@ def buildMinHash(shingle_list,num_perm=128):
   return mhash
 
 def ReadSongFiles(path, shingle_gram, hash_signatures, max_documents):
-  d_shingles = dict()
+  #d_shingles = dict()
   d_minhash = dict()
     
-  n_count = 0
+  #n_count = 0
   for r,d,f in os.walk(path):
     for file in f:
       pathf = r.replace('\\','/')
@@ -47,16 +47,18 @@ def ReadSongFiles(path, shingle_gram, hash_signatures, max_documents):
       if fshingle is None:
         continue
       
-      d_shingles[d_author_name] = fshingle
+      #d_shingles[d_author_name] = fshingle
 
       #-------------> MinHash
       d_minhash[d_author_name] = MinHash(num_perm = hash_signatures)
-      for d in d_shingles[d_author_name]:
+      #for d in d_shingles[d_author_name]:
+      for d in fshingle:
         d_minhash[d_author_name].update(d.encode('utf8'))
 	  
-      n_count = n_count + 1
-      if not (max_documents is None):
-        if n_count > max_documents:
-          return {'minhash' : d_minhash}
+      #n_count = n_count + 1
+      #if not (max_documents is None):
+      #  if n_count > max_documents:
+      #    return {'minhash' : d_minhash}
 
+  #{'shingle' : d_shingles, 'minhash' : d_minhash, 'number_of_signatures' : hash_signatures, 'n_gram' : shingle_gram}
   return {'minhash' : d_minhash}
