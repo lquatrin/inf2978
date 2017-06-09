7731ea85fb2d8c1d815543869131acad17f76c76
Leonardo Quatrin Campagnolo <campagnolo.lq@gmail.com>
2017-06-09 16:00:33 -0300

    arquivo de teste

--- t2/data_set_test.py
+++ t2/data_set_test.py
@@ -21,11 +21,12 @@ def duplicates(lsh_index):
   
   return possible_duplicates
 
-#######################################
-# Parametros
+#######################
+# Parametros MinHash
+#######################
 shingle_gram = [3, 4, 5]
 number_of_signatures = [30, 40, 50, 60, 70, 100]
-max_docs = 100
+max_docs = 1000
 
 for s_gram in shingle_gram:
   for n_sig in number_of_signatures:
@@ -42,8 +43,12 @@ for s_gram in shingle_gram:
       serialization.SavePickleObject('songdata_' + str(s_gram) + '_' + str(n_sig), d_results)
 
 
+###################
+# Parametros LSH
+###################
 similarity_threshold = [0.7, 0.8, 0.9]
 d_r_b = dict()
+# baseado no 'number_of_signatures', gerar as configuracoes
 d_r_b[30]  = [(3, 10), (5, 6), (6, 5)]
 d_r_b[40]  = [(4, 10), (5, 8), (8, 5)]
 d_r_b[50]  = [(5, 10), (2, 25)]
@@ -62,32 +67,33 @@ for s_gram in shingle_gram:
       assert(rows*bands == d_results['number_of_signatures'])	
   
       for s_threshold in similarity_threshold:
-        start = time.clock()
-  
-  	
         # Define "weights = (r*b / r, r*b / b)" or "params = (r, b)"
+        # Create LSH
         lsh = MinHashLSH(threshold = s_threshold, num_perm = rows*bands, params = (rows, bands))
         for k,v in d_results['minhash'].items():
           lsh.insert(k, v)
-
-        print("Create LSH", time.clock() - start)
         
         # Criando o arquivo csv
         ret_file = open('resfile_' + str(s_gram) + '_' + str(hash_sig) + '.csv','w')
         
-        # Checar letras parecidas baseado no valor de 'similarity_threshold'
-        #added_songs = dict()
-        #for key, v_minhash in d_results['minhash'].items():
-        #  if not key in added_songs:
-        #    result = lsh.query(v_minhash)
-        #
-        #    for i in range(len(result)):
-        #      added_songs[result[i]] = True
-        #    
-        #    if len(result) > 1:
-        #      ret_file.write(';'.join(result))
-        #      ret_file.write('\n')
-        #      #input(str(len(result)) + ': ' + (';'.join(map(str,result))))
+        start = time.clock()
+        print("Gerando Duplicatas")
+
+		# Checar letras parecidas baseado no valor de 'similarity_threshold'
+        added_songs = dict()
+        for key, v_minhash in d_results['minhash'].items():
+          if not key in added_songs:
+            result = lsh.query(v_minhash)
+        
+            for i in range(len(result)):
+              added_songs[result[i]] = True
+            
+            if len(result) > 1:
+              ret_file.write(';'.join(result))
+              ret_file.write('\n')
+              #input(str(len(result)) + ': ' + (';'.join(map(str,result))))
+
+        print(time.clock() - start)
         
         # Fechando arquivo CSV
         ret_file.close()
