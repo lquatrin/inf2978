8abb21a82057bcaef781c5bcfe711349043cf5ee
Leonardo Quatrin Campagnolo <campagnolo.lq@gmail.com>
2017-06-12 19:24:46 -0300

    arrumando parâmetros

--- t2/dataset_test.py
+++ t2/dataset_test.py
@@ -12,18 +12,14 @@ path = os.path.join(root, "TRAIN_DATASET/")
 #######################
 shingle_gram = [3, 4, 5]
 
-
+# baseado no 'number_of_signatures', gerar as configuracoes [30, 50, 75, 100]
 d_r_b = dict()
-# baseado no 'number_of_signatures', gerar as configuracoes
-number_of_signatures = [30, 40, 50, 60, 70, 100]
 d_r_b[30]  = [(3, 10), (5, 6), (6, 5)]
-d_r_b[40]  = [(4, 10), (5, 8), (8, 5)]
 d_r_b[50]  = [(5, 10), (2, 25)]
-d_r_b[60]  = [(6, 10), (5, 12)]
-d_r_b[70]  = [(7, 10), (10, 7)]
+d_r_b[75]  = [(7, 10), (10, 7)]
 d_r_b[100] = [(5, 20), (10, 10), (20, 5), (4, 25), (25, 4)]
 
-similarity_threshold = [0.7, 0.8, 0.9]
+similarity_threshold = [0.5, 0.6, 0.7, 0.8, 0.9]
 
 # for each shingle size
 for s_gram in shingle_gram:
